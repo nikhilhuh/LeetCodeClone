8a082aa47ceb5969d63a93806f8963bc6db17443
nikhil <nikhilhuh@gmail.com>
2024-07-01 13:39:39 +0530

    update

--- client-side/src/pages/SignIn.jsx
+++ client-side/src/pages/SignIn.jsx
@@ -6,28 +6,12 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { FacebookAuthProvider } from "firebase/auth";
 import { toast } from "react-toastify";
 
 function SignIn(props) {
   const handleGoogle = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
-      .then((res) => {
-        const user = res.user;
-        console.log("Logged In");
-        toast.success("Logged In");
-        navigate("/");
-      })
-      .catch((err) => {
-        toast.error("Error");
-        console.log(err.message);
-      });
-    };
-    
-    const handleFacebook = () => {
-      const provider1 = new FacebookAuthProvider();
-      signInWithPopup(auth, provider1)
       .then((res) => {
         const user = res.user;
         console.log("Logged In");
@@ -201,23 +185,6 @@ function SignIn(props) {
                 </svg>
               </span>
               Sign in with Google
-            </button>
-            <button
-            onClick={handleFacebook}
-              type="button"
-              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-            >
-              <span className="mr-2 inline-block">
-                <svg
-                  className="h-6 w-6 text-[#2563EB]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
-                </svg>
-              </span>
-              Sign in with Facebook
             </button>
           </div>
         </div>
--- client-side/src/pages/SignUp.jsx
+++ client-side/src/pages/SignUp.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Footer from "../components/Footer.jsx";
-import { NavLink, useLocation, useNavigate, useNavigation } from "react-router-dom";
+import { NavLink, useNavigate} from "react-router-dom";
 import { useState } from "react";
-import { FacebookAuthProvider, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-// import InputControl from "../components/inputControl";
+import {createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../firebase.js";
 import { GoogleAuthProvider,signInWithPopup } from "firebase/auth";
 import { toast } from "react-toastify";
@@ -25,20 +24,6 @@ function SignUp() {
       });
   };
 
-  const handleFacebook = () => {
-    const provider = new FacebookAuthProvider();
-    signInWithPopup(auth, provider)
-      .then((res) => {
-        const user = res.user;
-        toast.success("Signed Up Successfully");
-        console.log(user);
-        navigate("/");
-      })
-      .catch((err) => {
-        toast.error("Error Signing In");
-        console.log(err.message);
-      });
-  };
   const navigate = useNavigate();
   const [values, setValues] = useState({
     name: "",
@@ -232,23 +217,7 @@ function SignUp() {
               </span>
               Sign up with Google
             </button>
-            <button
-              onClick={handleFacebook}
-              type="button"
-              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-            >
-              <span className="mr-2 inline-block">
-                <svg
-                  className="h-6 w-6 text-[#2563EB]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
-                </svg>
-              </span>
-              Sign up with Facebook
-            </button>
+           
           </div>
         </div>
       </div>
